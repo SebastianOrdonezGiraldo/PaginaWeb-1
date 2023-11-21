a3a3bd7f3c3dd4aecd2b23752c03cf5b50da2bdb
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2023-11-21 10:44:07 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-// Importa la biblioteca React. Esta biblioteca es necesaria para crear componentes de React.
 import React from 'react';
-
-// Importa la biblioteca ReactDOM. Esta biblioteca es necesaria para renderizar los componentes de React en el DOM.
 import ReactDOM from 'react-dom';
-
-// Importa el componente App desde el archivo App.js. Este es el componente principal de tu aplicación.
 import App from './App';
 
-// Utiliza la función render de ReactDOM para renderizar el componente App en el elemento con id 'root' en tu archivo HTML.
-// <App /> es la sintaxis JSX para representar el componente App.
-// document.getElementById('root') obtiene una referencia al elemento con id 'root' en tu archivo HTML.
 ReactDOM.render(<App />, document.getElementById('root'));
